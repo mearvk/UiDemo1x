e0b5b5730ca14d21d1f831a67831e6eb905a0e63
Mr. Max Rupplin <mearvk@outlook.com>
2020-03-18 13:31:17 -0400

    Latest

--- src/system/ui/Builder.java
+++ src/system/ui/Builder.java
@@ -15,53 +15,40 @@ public class Builder
 
         JFrame jframe = (JFrame)Memory.ref.instance.pull(new Reference("//jframe"));
 
+            JMenuBar jmenubar = (JMenuBar)Memory.ref.instance.pull(new Reference("//"));
+
+                JMenu jmenu_file = (JMenu)Memory.ref.instance.pull(new Reference("//jmenu/file"));
+
+                    JMenuItem jmenuitem_file_1 = (JMenuItem)Memory.ref.instance.pull(new Reference("//jmenu/file/jmenuitem/1"));
+
+                    JMenuItem jmenuitem_file_2 = (JMenuItem)Memory.ref.instance.pull(new Reference("//jmenu/file/jmenuitem/2"));
+
+                JMenu jmenu_edit = (JMenu)Memory.ref.instance.pull(new Reference("//jmenu/edit"));
+
+                    JMenuItem jmenuitem_edit_1 = (JMenuItem)Memory.ref.instance.pull(new Reference("//jmenu/edit/jmenuitem/1"));
+
+                    JMenuItem jmenuitem_edit_2 = (JMenuItem)Memory.ref.instance.pull(new Reference("//jmenu/edit/jmenuitem/2"));
+
+                JMenu jmenu_view = (JMenu)Memory.ref.instance.pull(new Reference("//jmenu/view"));
+
+                    JMenuItem jmenuitem_view_1 = (JMenuItem)Memory.ref.instance.pull(new Reference("//jmenu/view/jmenuitem/1"));
+
+                    JMenuItem jmenuitem_view_2 = (JMenuItem)Memory.ref.instance.pull(new Reference("//jmenu/view/jmenuitem/2"));
+
             JPanel jpanel_top = (JPanel)Memory.ref.instance.pull(new Reference("//jpanel/top"));
 
-                Memory.ref.instance.pull(new Reference("//jtextpane/url"));
+                JTextPane jtextpane_url = (JTextPane)Memory.ref.instance.pull(new Reference("//jtextpane/url"));
 
-                Memory.ref.instance.pull(new Reference("//jtextpane/url"));
+                JButton jbutton_back = (JButton)Memory.ref.instance.pull(new Reference("//jbutton/back"));
 
-                Memory.ref.instance.pull(new Reference("//jtextpane/url"));
+                JButton jbutton_forward = (JButton)Memory.ref.instance.pull(new Reference("//jbutton/forward"));
 
-                Memory.ref.instance.pull(new Reference("//jtextpane/url"));
+                JButton jbutton_reload = (JButton)Memory.ref.instance.pull(new Reference("//jbutton/reload"));
 
-                Memory.ref.instance.pull(new Reference("//jtextpane/url"));
-
-                Memory.ref.instance.pull(new Reference("//jtextpane/url"));
-
-
-        Memory.ref.instance.push(new Reference("//jtextpane/url"), new JTextPane());
-
-        Memory.ref.instance.push(new Reference("//jbutton/back"), new JButton("back"));
-
-        Memory.ref.instance.push(new Reference("//jbutton/forward"), new JButton("forward"));
-
-        Memory.ref.instance.push(new Reference("//jbutton/reload"), new JButton("reload"));
-
-        Memory.ref.instance.push(new Reference("//jbutton/halt"), new JButton("halt"));
+                JButton jbutton_halt = (JButton) Memory.ref.instance.pull(new Reference("//jbutton/halt"));
 
             JPanel jpanel_bottom = (JPanel)Memory.ref.instance.pull(new Reference("//jpanel/bottom"));
 
-        //
-
-        JMenuBar jmenubar = (JMenuBar)Memory.ref.instance.pull(new Reference("//"));
-
-            JMenu jmenu_file = (JMenu)Memory.ref.instance.pull(new Reference("//jmenu/file"));
-
-                JMenuItem jmenuitem_file_1 = (JMenuItem)Memory.ref.instance.pull(new Reference("//jmenu/file/jmenuitem/1"));
-
-                JMenuItem jmenuitem_file_2 = (JMenuItem)Memory.ref.instance.pull(new Reference("//jmenu/file/jmenuitem/2"));
-
-            JMenu jmenu_edit = (JMenu)Memory.ref.instance.pull(new Reference("//jmenu/edit"));
-
-                JMenuItem jmenuitem_edit_1 = (JMenuItem)Memory.ref.instance.pull(new Reference("//jmenu/edit/jmenuitem/1"));
-
-                JMenuItem jmenuitem_edit_2 = (JMenuItem)Memory.ref.instance.pull(new Reference("//jmenu/edit/jmenuitem/2"));
-
-            JMenu jmenu_view = (JMenu)Memory.ref.instance.pull(new Reference("//jmenu/view"));
-
-                JMenuItem jmenuitem_view_1 = (JMenuItem)Memory.ref.instance.pull(new Reference("//jmenu/view/jmenuitem/1"));
-
-                JMenuItem jmenuitem_view_2 = (JMenuItem)Memory.ref.instance.pull(new Reference("//jmenu/view/jmenuitem/2"));
+                JEditorPane jeditorpane = (JEditorPane)Memory.ref.instance.pull(new Reference("//jeditorpane/html"));
     }
 }
