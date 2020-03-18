26ac03448dcd86c30b0d484637d00e668d26f707
Mr. Max Rupplin <mearvk@outlook.com>
2020-03-18 13:12:12 -0400

    Latest

--- src/Main.java
+++ src/Main.java
@@ -21,14 +21,34 @@ public class Main
 
         Memory.ref.instance.push(new Reference("//jpanel/bottom"), new JPanel());
 
-        Memory.ref.instance.push(new Reference("//jtextpane"), new JFrame());
+        Memory.ref.instance.push(new Reference("//jmenubar"), new JMenuBar());
 
-        Memory.ref.instance.push(new Reference("//button/back"), new JButton());
+        Memory.ref.instance.push(new Reference("//jmenu/file"), new JMenu("file"));
 
-        Memory.ref.instance.push(new Reference("//button/forward"), new JButton());
+            Memory.ref.instance.push(new Reference("//jmenu/file/jmenuitem/1"), new JMenu("item1"));
 
-        Memory.ref.instance.push(new Reference("//button/reload"), new JButton());
+            Memory.ref.instance.push(new Reference("//jmenu/file/jmenuitem/2"), new JMenu("item2"));
 
-        Memory.ref.instance.push(new Reference("//button/stop"), new JButton());
+        Memory.ref.instance.push(new Reference("//jmenu/edit"), new JMenu("edit"));
+
+            Memory.ref.instance.push(new Reference("//jmenu/edit/jmenuitem/1"), new JMenu("item1"));
+
+            Memory.ref.instance.push(new Reference("//jmenu/edit/jmenuitem/2"), new JMenu("item2"));
+
+        Memory.ref.instance.push(new Reference("//jmenu/view"), new JMenu("view"));
+
+            Memory.ref.instance.push(new Reference("//jmenu/view/jmenuitem/1"), new JMenu("item1"));
+
+            Memory.ref.instance.push(new Reference("//jmenu/view/jmenuitem/2"), new JMenu("item2"));
+
+        Memory.ref.instance.push(new Reference("//jtextpane/url"), new JTextPane());
+
+        Memory.ref.instance.push(new Reference("//jbutton/back"), new JButton("back"));
+
+        Memory.ref.instance.push(new Reference("//jbutton/forward"), new JButton("forward"));
+
+        Memory.ref.instance.push(new Reference("//jbutton/reload"), new JButton("reload"));
+
+        Memory.ref.instance.push(new Reference("//jbutton/halt"), new JButton("halt"));
     }
 }
--- src/system/instance/SystemInstance.java
+++ src/system/instance/SystemInstance.java
@@ -13,6 +13,4 @@ public class SystemInstance
     {
         this.reference = reference;
     }
-
-
 }
