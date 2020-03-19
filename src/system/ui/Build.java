07f83bcf429b9dcea8b5989b07be7778e7c891b8
Mr. Max Rupplin <mearvk@outlook.com>
2020-03-19 09:28:59 -0400

    Latest

--- src/system/ui/Build.java
+++ src/system/ui/Build.java
@@ -32,7 +32,7 @@ public class Build
 
     public JMenuItem jmenuitem_view_2;
 
-    public JPanelTop jpanel_top;
+    public TopPane jpanel_top;
 
     public JTextPane jtextpane_url;
 
@@ -44,9 +44,9 @@ public class Build
 
     public JButton jbutton_halt;
 
-    public JPanelBottom jpanel_bottom;
+    public BottomPane jpanel_bottom;
 
-    public JEditorPaneHTML jeditorpane;
+    public HTMLPane jeditorpane;
 
     public Build()
     {
@@ -59,9 +59,9 @@ public class Build
     {
         Memory.push(new Name("//jframe"), new JFrame());
 
-        Memory.push(new Name("//jpanel/top"), new JPanelTop());
+        Memory.push(new Name("//jpanel/top"), new TopPane());
 
-        Memory.push(new Name("//jpanel/bottom"), new JPanelBottom());
+        Memory.push(new Name("//jpanel/bottom"), new BottomPane());
 
         Memory.push(new Name("//jmenubar"), new JMenuBar());
 
@@ -93,7 +93,7 @@ public class Build
 
         Memory.push(new Name("//jbutton/halt"), new JButton("halt"));
 
-        Memory.push(new Name("//jeditorpane/html"), new JEditorPaneHTML());
+        Memory.push(new Name("//jeditorpane/html"), new HTMLPane());
 
         //
 
@@ -119,7 +119,7 @@ public class Build
 
         this.jmenuitem_view_2 = (JMenuItem)Memory.pull(new Name("//jmenu/view/jmenuitem/2"));
 
-        this.jpanel_top = (JPanelTop)Memory.pull(new Name("//jpanel/top"));
+        this.jpanel_top = (TopPane)Memory.pull(new Name("//jpanel/top"));
 
         this.jtextpane_url = (JTextPane)Memory.pull(new Name("//jtextpane/url"));
 
@@ -131,9 +131,9 @@ public class Build
 
         this.jbutton_halt = (JButton) Memory.pull(new Name("//jbutton/halt"));
 
-        this.jpanel_bottom = (JPanelBottom) Memory.pull(new Name("//jpanel/bottom"));
+        this.jpanel_bottom = (BottomPane) Memory.pull(new Name("//jpanel/bottom"));
 
-        this.jeditorpane = (JEditorPaneHTML) Memory.pull(new Name("//jeditorpane/html"));
+        this.jeditorpane = (HTMLPane) Memory.pull(new Name("//jeditorpane/html"));
 
         return this;
     }
@@ -193,7 +193,7 @@ public class Build
 
     public Build postbuild()
     {
-        this.jframe.setSize(new Dimension(800,600));
+        this.jframe.setSize(new Dimension(1000,600));
 
         this.jframe.setLayout(new FlowLayout());
 
@@ -212,6 +212,44 @@ public class Build
         this.jframe.setVisible(true);
 
         this.jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //this.jeditorpane.setContentType();
+
+        this.jeditorpane.setContentType("text/html");
+
+        this.jeditorpane.setText("<html>\n" +
+                "   <head>\n" +
+                "      <title>Swing Tester</title>\n" +
+                "   </head>\n" +
+                "   <body>\n" +
+                "      <p>Swing API is a set of extensible GUI Components \n" +
+                "         to ease the developer's life to create JAVA based Front End/GUI\n" +
+                "         Applications. It is build on top of AWT API and acts as a replacement of \n" +
+                "         AWT API, since it has almost every control corresponding to AWT controls.\n" +
+                "         Swing component follows a Model-View-Controller architecture to \n" +
+                "         fulfill the following criterias.</p>\n" +
+                "      <ul>\n" +
+                "         <li><p>A single API is to be sufficient to support multiple look and feel.</p></li>\n" +
+                "         <li><p>API is to be model driven so that the highest level API is not required to have data.</p></li>\n" +
+                "         <li><p>API is to use the Java Bean model so that Builder Tools and IDE can provide better services to the developers for use.</p></li>\n" +
+                "      </ul>\n" +
+                "      <h2>MVC Architecture</h2>\n" +
+                "      <p>Swing API architecture follows loosely based MVC architecture in the following manner.</p>\n" +
+                "      <ul>\n" +
+                "         <li><p>Model represents component's data.</p></li>\n" +
+                "         <li><p>View represents visual representation of the component's data.</p></li>\n" +
+                "         <li><p>Controller takes the input from the user on the view and reflects the changes in Component's data.</p></li>\n" +
+                "         <li><p>Swing component has Model as a seperate element, while the View and Controller part are clubbed in the User Interface elements. Because of which, Swing has a pluggable look-and-feel architecture.</p></li>\n" +
+                "      </ul>\n" +
+                "      <h2>Swing Features</h2>\n" +
+                "      <ul>\n" +
+                "         <li><p><b>Light Weight</b> − Swing components are independent of native Operating System's API as Swing API controls are rendered mostly using pure JAVA code instead of underlying operating system calls.</p></li>\n" +
+                "         <li><p><b>Rich Controls</b> − Swing provides a rich set of advanced controls like Tree, TabbedPane, slider, colorpicker, and table controls.</p></li>\n" +
+                "         <li><p><b>Highly Customizable</b> − Swing controls can be customized in a very easy way as visual apperance is independent of internal representation.</p></li>\n" +
+                "         <li><p><b>Pluggable look-and-feel</b> − SWING based GUI Application look and feel can be changed at run-time, based on available values.</p></li>\n" +
+                "      </ul>\n" +
+                "   </body>\n" +
+                "</html>");
 
         return this;
     }
--- src/system/ui/HTMLPane.java
+++ src/system/ui/HTMLPane.java
@@ -6,16 +6,16 @@ import system.naming.Name;
 import javax.swing.*;
 import java.awt.*;
 
-public class JEditorPaneHTML extends JEditorPane
+public class HTMLPane extends JEditorPane
 {
     @Override
     public Dimension getPreferredSize()
     {
         JFrame jframe = (JFrame) Memory.pull(new Name("//jframe"));
 
-        int height = jframe.getHeight() - 100;
+        int height = jframe.getHeight() - 150;
 
-        int width = jframe.getWidth() - 10;
+        int width = jframe.getWidth() - 36;
 
         return new Dimension(width, height);
     }
